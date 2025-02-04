c397fb411c0724c186cca3844fb0216669c6c8f8
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-02-04 23:42:59 +0600

    Update Maximum Product Subarray-leetcode.py

--- Maximum Product Subarray-leetcode.py
+++ Maximum Product Subarray-leetcode.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        positive, negative = nums[0], nums[0]
+        result = nums[0]
+        for num in nums[1:]:
+            positive, negative = max(num, positive * num, negative * num), min(num,
+                                                                               positive * num, negative * num)
+            result = max(result, positive)
+        return result
 
+
+if __name__ == "__main__":
+    assert Solution().maxProduct([2, 3, -2, 4]) == 6
